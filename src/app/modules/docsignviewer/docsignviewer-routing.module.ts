d66ea54a33befc744c4d26ee34bf0ef10c3ecfbf
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-09-14 16:22:23 +0530

    authGuard added

--- src/app/modules/docsignviewer/docsignviewer-routing.module.ts
+++ src/app/modules/docsignviewer/docsignviewer-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Prerequisite } from '../../service/prerequisite';
 import {ViewerComponent} from '../docsignviewer/viewer/viewer.component';
 import {ErrorpageComponent} from '../docsignviewer/errorpage/errorpage.component';
+import {AuthGuard} from  '../../service/authguard.service';
 import { from } from 'rxjs';
 
 const routes: Routes = [
@@ -15,7 +16,7 @@ const routes: Routes = [
         {
           path: ':cmpid/:envid',
           component: ViewerComponent,
-        
+          canActivate: [AuthGuard],
           data: {
             title: 'Sign',
             code: 'sign'
@@ -25,7 +26,7 @@ const routes: Routes = [
         {
           path: ':cmpid/:envid/:docid',
           component: ViewerComponent,
-        
+          canActivate: [AuthGuard],
           data: {
             title: 'Sign',
             code: 'sign'
@@ -36,7 +37,7 @@ const routes: Routes = [
         {
           path: 'error',
           component: ErrorpageComponent,
-        
+          canActivate: [AuthGuard],
           data: {
             title: 'Error',
             code: 'error'
