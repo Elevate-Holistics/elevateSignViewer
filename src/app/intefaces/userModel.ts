a48e8de920db628aa3009cd4f58ef5c84cd62a16
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-09-14 16:22:04 +0530

    changes made in usermodal

--- src/app/intefaces/userModel.ts
+++ src/app/intefaces/userModel.ts
@@ -1,12 +1,15 @@
 export interface UserModel {
-    id: number;
-    name: string;
-    menu?: any;
-    token: any;
+    //     id: number;
+    //     name: string;
+    //     menu?: any;
+    //     token: any;
 
-    
-  
-    cmp?: string;
-    usertype?: number;
-    rolename?: string;
+
+
+    //     cmp?: string;
+    //     usertype?: number;
+    //     rolename?: string;
+    id :string;
+    email: string;
+    key: string;
 }
