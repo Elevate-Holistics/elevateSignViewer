9c4e7f46b05d20799b307912c63d03ef0b6d0503
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-09-23 18:10:21 +0530

    binding dynamic message

--- src/app/modules/docsignviewer/completed/completed.component.ts
+++ src/app/modules/docsignviewer/completed/completed.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-completed',
   templateUrl: './completed.component.html',
   styleUrls: ['./completed.component.css']
 })
 export class CompletedComponent implements OnInit {
-
-  constructor() { }
+  message:any='';
+  type:any='';
+  constructor( private router: Router,private activatedRoute: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.type = this.activatedRoute.snapshot.paramMap.get('type');
+    
   }
-
+ngAfterViewInit(): void {
+  //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+  //Add 'implements AfterViewInit' to the class.
+  if(this.type =='f'){
+this.message='Document Completed!'
+  }else if(this.type =='a'){
+    this.message='You Have Already Completed The Document !'
+  }
+}
 }
