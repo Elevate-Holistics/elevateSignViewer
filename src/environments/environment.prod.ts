77687be14762ede8a97eedb3ac673bbe34daf9db
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-09-25 18:37:12 +0530

    AWS_BUCKET_PREFIX addes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,9 +10,10 @@
 
 export const environment = {
   "production": true,
-  "api_root": "http://ec2-18-191-238-218.us-east-2.compute.amazonaws.com:81/ELEVATEPHP/public/index1.php/api",
+  "api_root": "http://ec2-54-174-155-193.compute-1.amazonaws.com:80/ELEVATEPHP/public/index1.php/api",
   "upload_folder": "uploaddoc",
   "directUpload": "upload",
   "document_path": "http://ec2-18-191-238-218.us-east-2.compute.amazonaws.com:81/ELEVATEPHP/documents",
-  "cloudinary_url": "https://res.cloudinary.com/djuzgpzo8/image/upload/"
+  "cloudinary_url": "https://res.cloudinary.com/djuzgpzo8/image/upload/",
+  "AWS_BUCKET_PREFIX":"elevate-stage-"
 };
