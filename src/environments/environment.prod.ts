eca7e8b7b38338e567cc5c468a557441d210ddfd
MOORTHI <moorthi@ideas2tech.com>
2020-09-29 14:53:45 +0530

    port change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   "production": true,
-  "api_root": "http://ec2-54-174-155-193.compute-1.amazonaws.com:81/ELEVATEPHP/public/index1.php/api",
+  "api_root": "http://ec2-54-174-155-193.compute-1.amazonaws.com/ELEVATEPHP/public/index1.php/api",
   "upload_folder": "uploaddoc",
   "directUpload": "upload",
-  "document_path": "http://ec2-54-174-155-193.compute-1.amazonaws.com:81/ELEVATEPHP/documents",
+  "document_path": "http://ec2-54-174-155-193.compute-1.amazonaws.com/ELEVATEPHP/documents",
   "cloudinary_url": "https://res.cloudinary.com/djuzgpzo8/image/upload/",
   "AWS_BUCKET_PREFIX": "elevate-stage-"
 };
