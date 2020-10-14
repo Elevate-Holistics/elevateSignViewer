affd9f1c41a092aa0f55b24195d6a6fa13707977
MOORTHI <moorthi@ideas2tech.com>
2020-10-14 17:21:35 +0530

    prod api path change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   "production": true,
-  "api_root": "http://ec2-54-174-155-193.compute-1.amazonaws.com/ELEVATEPHP/public/index1.php/api",
+  "api_root": "https://esignapi.elevate-holistics.com/ELEVATEPHP/public/index1.php/api",
   "upload_folder": "uploaddoc",
   "directUpload": "upload",
   "document_path": "http://ec2-54-174-155-193.compute-1.amazonaws.com/ELEVATEPHP/documents",
