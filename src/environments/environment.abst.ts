aec40c55f6cc7f5f6a71104984dc3833c7f82648
MOORTHI <moorthi@ideas2tech.com>
2020-11-30 14:27:24 +0530

    package changes

---
+++ src/environments/environment.abst.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    "production": true,
+    "api_root": "http://54.163.218.186/ELEVATEPHP/public/index1.php/api",
+    "upload_folder": "uploaddoc",
+    "directUpload": "upload",
+    "document_path": "http://54.163.218.186/ELEVATEPHP/public/upload/",
+    "cloudinary_url": "https://res.cloudinary.com/djuzgpzo8/image/upload/",
+    "AWS_BUCKET_PREFIX": "https://c972288dda594a4b8b0b72927ed29703-cmp{0}.s3.us-east-1.amazonaws.com/"
+  };
